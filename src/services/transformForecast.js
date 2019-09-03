1b5cc69e8014dff9b6f2787ddbbb7347abc94bfd
Oscar Osses <oscar.osses@direcciondecompras.local>
2019-09-03 15:40:08 -0400

    99. Uso de Map, y Culturización con Moment Locale

--- src/services/transformForecast.js
+++ src/services/transformForecast.js
@@ -1,3 +1,18 @@
-const transformForecast = data => ({});
+import moment from 'moment';
+import transformWeather from './transformWeather';
+
+const transformForecast = data => (
+   data.list.filter(item => (
+      moment.unix(item.dt).hour() === 6 ||
+      moment.unix(item.dt).hour() === 12 ||
+      moment.unix(item.dt).hour() === 18
+   )).map(item => (
+      {
+         weekDay: moment.unix(item.dt).format('ddd'),
+         hour: moment.unix.apply(item.dt).hour(),
+         data: transformWeather(item)
+      }
+   ))
+);
 
 export default transformForecast;
