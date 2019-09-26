bfc8381086e70a880a4e608e4e003c523b5bd7f7
Oscar Osses <oscar.osses@direcciondecompras.local>
2019-09-26 10:53:37 -0300

    129. Incorporación de middlware Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,28 @@
 export const SET_CITY = 'SET_CITY';
 
 export const setCity = value => ({ type: SET_CITY, value });
+
+const api_key = 'cc6aae116a67355c8d5c5772051cb35d';
+const url = 'http://api.openweathermap.org/data/2.5/forecast';
+
+export const fetchForecast = payload => {
+   return dispatch => {
+      const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+      //console.log(url_forecast);
+      
+      // activar en el estado un indicador de búsqueda de datos
+
+      fetch(url_forecast).then(
+         data => (data.json())
+      ).then(
+         weather_data => {
+            //console.log(weather_data);
+            const forecastData = transformForecast(weather_data);
+            //console.log(forecastData);
+            //this.setState({ forecastData: forecastData })
+
+            // modificar el estado con el resultado de la promise (fetch)
+         }
+      );
+   }
+}
